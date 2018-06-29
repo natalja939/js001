1daaece15bbfc031988c543cb3963e2991283f7e
NataliiaB <bejbanatalja939@gmail.com>
2018-06-29 16:01:56 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// const msg = 'Hello World!';
+// alert(msg);
+
+// const productTitle = 'Tesla X'; // String
+// const productPrice = 150000; // Number
+// const productAvailable = true; // Boolean
+// alert(productTitle);
+// alert(productPrice);
+// alert(productAvailable);
+
+// undefined
+// null
+// Object => Function, Array
+// Symbol
+
+// const userName = prompt('Enter your name');
+// const msg = 'Hello, ' + userName;
+// alert(msg);
+
+//const userAge = parseFloat(prompt('Enter your age'));
+// const newAge = userAge + 1;
+// const msgAge = 'Next year you will be ' + newAge;
+// alert(msgAge);
+
+// if (userAge == 18) {
+//     alert('You are adult');
+// } else {
+//     alert('You are child');
+// }
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+const amount = sum(42, 43, 5);
+alert(amount);
+
+const total = sum(100, "Hello", "Hey");
+alert(total);
